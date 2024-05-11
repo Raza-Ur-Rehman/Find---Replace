84ef8f6f2897737001ccd882d2d709cc5af27806
Raza ur Rehman <mrazaurrehman12345@gmail.com>
2024-05-12 01:33:35 +0500

    FINAL FIXED CODE

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
+// function findReplace() {
+//     let para = document.getElementById('values');
+//     let findWord = document.getElementById('find');
+//     let ReplaceWord = document.getElementById('replace');
+//     let userPara = document.getElementById('userpara');
+//     let update = document.getElementById('update');
+
+//     let replacedValue = para.value.replace(findWord.value, ReplaceWord.value);
+//     userPara.innerText = para.value;
+//     update.innerText = replacedValue;
+// }
+
+
+
 function findReplace() {
+
     let para = document.getElementById('values');
     let findWord = document.getElementById('find');
     let ReplaceWord = document.getElementById('replace');
-    let userpara = document.getElementById('userpara');
     let update = document.getElementById('update');
+    let userPara = document.getElementById('userpara');
 
-    let replacedValue = para.value.replace(findWord.value, ReplaceWord.value);
-    userpara.innerText = para.value;
+
+    let replacedValue = para.value.replaceAll(" " + findWord.value + " ", " " + ReplaceWord.value + " ");
+    userPara.innerText = para.value;
     update.innerText = replacedValue;
 }
